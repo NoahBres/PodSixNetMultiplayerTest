8469d293ec5d9d784aa1ed472e12e883cc583a36
Noah <noahbres@gmail.com>
2019-01-23 13:00:14 -0600

    Update client_channel.py

--- PodSixMultiplayerTest/backend/client_channel.py
+++ PodSixMultiplayerTest/backend/client_channel.py
@@ -5,10 +5,20 @@ class ClientChannel(Channel):
     def __init__(self, *args, **kwargs):
         self.nickname = "anonymous"
         Channel.__init__(self, *args, **kwargs)
+        self.id = str(self._server.get_unique_id())
+        # intid = int(self.id)
+        # self.color = [(intid + 1) % 3 * 84, (intid + 2) % 3 * 84, (intid + 3) % 3 * 84]
 
     def Network(self, data):
         print(data)
+        pass
 
-    def Network_myaction(self, data):
-        print(data)
+    def Close(self):
+        self._server.delete_player(self)
+
+    # def Network_myaction(self, data):
+    #     print(data)
+
+    def Network_update(self, data):
+        self._server.move_player(data)
 
