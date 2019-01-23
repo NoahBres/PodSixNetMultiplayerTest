8fdd7ec0a41f648a855e310fa4374cfb632da4f3
Noah <noahbres@gmail.com>
2019-01-23 13:00:06 -0600

    Update client.py

--- PodSixMultiplayerTest/client/client.py
+++ PodSixMultiplayerTest/client/client.py
@@ -1,15 +1,45 @@
 from PodSixNet.Connection import connection, ConnectionListener
+from sys import exit
 
 
 class Client(ConnectionListener):
-    def __init__(self):
+    def __init__(self, game):
         self.Connect(("localhost", 1234))
 
-        connection.Send({"action": "myaction", "blah": "Test", "things": [1, 3, 4]})
+        self.game = game
+        self.id = None
 
-    def Loop(self):
+    def loop(self):
         connection.Pump()
         self.Pump()
 
+    def Network(self, data):
+        print(data)
+
     def Network_connected(self, data):
         print("connected")
+
+    def Network_disconnected(self, data):
+        print("Server disconnected")
+        # exit()
+
+    def Network_error(self, data):
+        print(f"Error: {data['error'][1]}")
+
+    def Network_set_id(self, data):
+        print(f"Setting id: {data['data']}")
+        self.game.set_client_id(data["data"])
+        self.id = data["data"]
+
+    def Network_players(self, data):
+        print("Players update: " + ", ".join(p for p in data["players"]))
+        self.game.set_players(data["players"])
+
+    def Network_del_player(self, data):
+        self.game.del_player(data)
+
+    def Network_update(self, data):
+        self.game.move_players(data)
+
+    def send(self, action, data):
+        connection.Send({"action": action, "origin": self.id, "data": data})
